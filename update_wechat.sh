5fcc73b9573f6f1c8ef45aedaf6217da5daa524f
byml-c <2014078563@qq.com>
2026-04-09 00:16:50 +0800

    update

--- update_wechat.sh
+++ update_wechat.sh
@@ -2,7 +2,16 @@
 git pull --no-rebase
 
 jpg_file=$(ls . | grep 'jpg')
-mv $jpg_file ./docs/community/images/wechat-group.jpg
+if [ -z "$jpg_file" ]; then
+    png_file=$(ls . | grep 'png')
+    if [ -z "$png_file" ]; then
+        echo "No jpg or png file found in the current directory."
+        exit 1
+    fi
+    mv $png_file ./docs/community/images/wechat-group.jpg
+else
+    mv $jpg_file ./docs/community/images/wechat-group.jpg
+fi
 
 mkdocs gh-deploy --remote-name origin --remote-branch gh-pages
 git add .
